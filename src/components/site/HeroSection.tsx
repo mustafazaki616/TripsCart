f4dd0785061cdb384ead2761aee76c99ff89396b
mustafazaki616 <168564443+mustafazaki616@users.noreply.github.com>
2025-09-17 01:31:03 +0500

    text change

--- src/components/site/HeroSection.tsx
+++ src/components/site/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
       </div>
       <div className="relative container mx-auto pt-16 md:pt-28 pb-10 md:pb-16 px-2 sm:px-6">
         <div className="max-w-3xl ">
-          <h1 className="text-gradient-primary text-2xl md:text-4xl lg:text-5xl">FILL THE CART, FLY THE WORLD</h1>
+          <h1 className="text-gradient-primary text-2xl md:text-4xl lg:text-5xl">Your trip, just a click away</h1>
           <p className="mt-4 text-base md:text-lg lg:text-xl text-muted-foreground max-w-2xl">
             Explore a vast array of options and find the perfect tickets with our easy online booking.
           </p>
